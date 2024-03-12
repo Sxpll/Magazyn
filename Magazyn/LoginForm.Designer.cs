f69e5122888c43ed495d5adaa41c7cedf1d1d886
Bartłomiej Sobczak <UL0273270@edu.uni.lodz.pl>
2024-03-12 23:13:02 +0100

    poprawa wizualna

--- Magazyn/LoginForm.Designer.cs
+++ Magazyn/LoginForm.Designer.cs
@@ -148,7 +148,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(110, 105);
+            this.pictureBox1.Location = new System.Drawing.Point(113, 105);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(100, 95);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
